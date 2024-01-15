3eb2b97bc518ed1d2225582b1826ffdca47f2d4c
nicolasjott <nicobox2002@gmail.com>
2024-01-15 17:32:20 -0500

    update site

--- tesync.com/astro.config.mjs
+++ tesync.com/astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tesync.github.io",
-  base: "/tesync.com",
+  site: "https://nicolasjott.github.io",
 });
