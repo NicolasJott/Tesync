43ae6c834e1c03c27296d109340631d260b6c3e3
nicolasjott <nicobox2002@gmail.com>
2024-01-21 13:51:16 -0500

    Update site URL in astro.config.mjs

--- tesync.com/astro.config.mjs
+++ tesync.com/astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nicolasjott.github.io",
+  site: "https://tesync.github.io",
   base: "/tesync.com",
 });
