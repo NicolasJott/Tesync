b8cd81edd27acda1cbfe516aed3d5e213ebd8120
nicolasjott <nicobox2002@gmail.com>
2024-01-21 19:35:09 -0500

    set axios headers and refresh token implementation

--- tesync-expo/app/(app)/_layout.tsx
+++ tesync-expo/app/(app)/_layout.tsx
@@ -1,9 +1,10 @@
+import { useMountEffect } from "@react-hookz/web/src/useMountEffect";
 import { Redirect, Stack } from "expo-router";
 import { Text } from "tamagui";
 import { useSession } from "../../components/context/TeslaSession";
 
 export default function AppLayout() {
-  const { session, isLoading } = useSession();
+  const { session, isLoading, refresh } = useSession();
 
   // You can keep the splash screen open, or render a loading screen like we do here.
   if (isLoading) {
@@ -17,6 +18,12 @@ export default function AppLayout() {
     // in the headless Node process that the pages are rendered in.
     return <Redirect href="/sign-in" />;
   }
+
+  useMountEffect(() => {
+    if (session) {
+      refresh();
+    }
+  });
 
   // This layout can be deferred because it's not the root layout.
   return (
--- tesync-expo/components/context/TeslaSession.tsx
+++ tesync-expo/components/context/TeslaSession.tsx
@@ -1,23 +1,35 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import * as WebBrowser from "expo-web-browser";
 import queryString from "query-string";
 import React from "react";
 import { useStorageState } from "./useStorageState";
 
+type AuthTokens = {
+  access_token: string;
+  refresh_token: string;
+};
+
 const AuthContext = React.createContext<{
   signIn: () => void;
   signOut: () => void;
+  refresh: () => void;
   session?: string | null;
   isLoading: boolean;
 }>({
   signIn: () => null,
   signOut: () => null,
+  refresh: () => null,
   session: null,
   isLoading: false,
 });
 
-const client_id = encodeURIComponent("0ff2ce810511-4dd3-8bbe-1a8b50cebdd2");
-const client_secret = encodeURIComponent("ta-secret.bhpd5JiQV0c1@Pb&");
+const client_id = encodeURIComponent(
+  process.env.EXPO_PUBLIC_TESLA_CLIENT_ID as string
+);
+const client_secret = encodeURIComponent(
+  process.env.EXPO_PUBLIC_TESLA_CLIENT_SECRET as string
+);
 const state = encodeURIComponent(process.env.EXPO_PUBLIC_TESLA_STATE as string);
 const response_type = encodeURIComponent("code");
 const scope = encodeURIComponent(
@@ -30,9 +42,7 @@ const redirect_uri = encodeURIComponent(
 const audience = encodeURIComponent(
   "https://fleet-api.prd.na.vn.cloud.tesla.com"
 );
-const AuthorizeUrl = `https://auth.tesla.com/oauth2/v3/authorize?response_type=${encodeURIComponent(
-  response_type
-)}&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&state=${state}`;
+const AuthorizeUrl = `https://auth.tesla.com/oauth2/v3/authorize?response_type=${response_type}&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&state=${state}`;
 
 // This hook can be used to access the user info.
 export function useSession() {
@@ -63,14 +73,14 @@ export function TeslaSessionProvider(props: React.PropsWithChildren) {
   };
 
   const GetTeslaAccessToken = async (code: string) => {
-    const data = `grant_type=authorization_code&client_id=${client_id}&client_secret=${client_secret}&code=${encodeURIComponent(
+    const tokenRequestData = `grant_type=authorization_code&client_id=${client_id}&client_secret=${client_secret}&code=${encodeURIComponent(
       code
     )}&redirect_uri=${redirect_uri}&audience=${audience}`;
 
     try {
       const response = await axios.post(
         "https://auth.tesla.com/oauth2/v3/token",
-        data,
+        tokenRequestData,
         {
           headers: {
             "Content-Type": "application/x-www-form-urlencoded",
@@ -78,7 +88,9 @@ export function TeslaSessionProvider(props: React.PropsWithChildren) {
         }
       );
 
-      setSession(response.data.access_token);
+      const { access_token, refresh_token } = response.data;
+
+      await setTokens(access_token, refresh_token);
     } catch (error) {
       console.error(error);
     }
@@ -91,6 +103,46 @@ export function TeslaSessionProvider(props: React.PropsWithChildren) {
     }
   };
 
+  const RefreshTeslaAccessToken = async () => {
+    console.log("Refreshing Tesla Access Token");
+    const refresh_token = await AsyncStorage.getItem("refresh_token");
+
+    if (refresh_token) {
+      const encodedRefreshToken = encodeURIComponent(refresh_token);
+      const tokenRequestData = `grant_type=refresh_token&client_id=${client_id}&refresh_token=${encodedRefreshToken}`;
+
+      try {
+        const response = await axios.post(
+          "https://auth.tesla.com/oauth2/v3/token",
+          tokenRequestData,
+          {
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+          }
+        );
+
+        const { access_token, refresh_token } = response.data;
+
+        await setTokens(access_token, refresh_token);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  const setTokens = async (access_token: string, refresh_token: string) => {
+    await AsyncStorage.setItem("refresh_token", refresh_token);
+    axios.defaults.headers.common["Authorization"] = `Bearer ${access_token}`;
+    setSession(access_token);
+  };
+
+  const signOut = async () => {
+    await AsyncStorage.removeItem("refresh_token");
+    axios.defaults.headers.common["Authorization"] = "";
+    setSession(null);
+  };
+
   return (
     <AuthContext.Provider
       value={{
@@ -98,7 +150,10 @@ export function TeslaSessionProvider(props: React.PropsWithChildren) {
           TeslaOAuth();
         },
         signOut: () => {
-          setSession(null);
+          signOut();
+        },
+        refresh: () => {
+          RefreshTeslaAccessToken();
         },
         session,
         isLoading,
